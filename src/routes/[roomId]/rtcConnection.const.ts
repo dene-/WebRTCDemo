69c37611fbb6cc39aa862bedc0e897bd64a01d85
Den <denis.nebot@europcar.com>
2024-05-14 09:07:04 +0200

    Add more stun servers.

--- src/routes/[roomId]/rtcConnection.const.ts
+++ src/routes/[roomId]/rtcConnection.const.ts
@@ -1,5 +1,9 @@
 export const iceServers = [
   { urls: 'stun:stun.l.google.com:19302' },
+  { urls: 'stun:stun1.l.google.com:19302' },
+  { urls: 'stun:stun2.l.google.com:19302' },
+  { urls: 'stun:stun3.l.google.com:19302' },
+  { urls: 'stun:stun4.l.google.com:19302' },
   { 
     urls: 'turn:turn.terminaldogma.win:1985',
     username: 'fulanito',
